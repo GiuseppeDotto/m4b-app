15961248439bafa053b37bf1ae4e1875de849fee
Giuseppe Dotto <gppdtt@gmail.com>
2025-06-14 15:41:40 +0200

    finished feature

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,14 @@
+import ObjectToTable from "./ObjectToTable";
+
 export default function Home() {
+  const posts = [{ title: "qwerty", votes: 10, thumb: "/thumbs/post1.png" }];
+
   return (
     <>
       <h1>Macro4BIM</h1>
       <h2>blog home page</h2>
+      <h3>post list</h3>
+      <ObjectToTable obj={posts} />
     </>
   );
 }
